17253d333fdbe6ea1e929df02e9401c3ad2dd3aa
Andrey Tvorozhkov <atvorozhkov@yandex.ru>
2017-01-11 14:49:39 -0500

    add wait and fix learner call

--- demo_run.sh
+++ demo_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bash run_redis.sh
+sleep 30
 python player.py experiment &
 python player.py experiment &
 python player.py experiment &
 python player.py experiment &
-THEANO_FLAGS=device=gpu learner.py experiment
+THEANO_FLAGS=device=gpu python learner.py experiment
